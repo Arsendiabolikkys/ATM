d17982ef315a911dcdcf4c34fbf203111ee7d53d
Arsendiabolikkys <diabolikkys@gmail.com>
2015-06-25 20:10:12 +0300

    card input

--- ATM/ATM/Classes/ATM.cs
+++ ATM/ATM/Classes/ATM.cs
@@ -12,11 +12,15 @@ namespace ATM.Classes
         private int RatingsCount;
 
         private List<Cash> Money;
-        private Card CurrentCard;     
+        private List<Card> AvailableCards;
+        private Card CurrentCard;
+   
 
         public ATM()
         {
             Money = new List<Cash>();
+            AvailableCards = new List<Card>();
+
             string path = @"data\ATM.txt";
             string text;
             using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
@@ -27,11 +31,58 @@ namespace ATM.Classes
                 }
                 RatingsCount = Money.Count;
             }
+
+            path = @"data\Cards.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                while (!String.IsNullOrEmpty(text = sr.ReadLine()))
+                {
+                    AvailableCards.Add(new Card(text, sr.ReadLine(), Double.Parse(sr.ReadLine())));
+                }
+            }
         }
 
         public void RunATM()
         {
-            
+            Console.WriteLine("card number - ");
+            string cardNumber = Console.ReadLine();
+            if (CardExist(cardNumber))
+            {
+                Console.WriteLine("password - ");
+                string password = Console.ReadLine();
+                if (IsValidPassword(cardNumber, password))
+                {
+                    CurrentCard = AvailableCards.Find(x => x.CardNumber.Equals(cardNumber));
+                    Menu();
+                }
+                else 
+                {
+                    Console.WriteLine("wrong pass");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("card is not exist");
+            }
+        }
+
+        private void Menu()
+        {
+
+        }
+
+        private bool CardExist(string cardNumber)
+        {
+            foreach (var card in AvailableCards)
+            {
+                if (card.CardNumber == cardNumber) return true;
+            }
+            return false;
+        }
+
+        private bool IsValidPassword(string cardNumber, string password)
+        {
+            return AvailableCards.Find(x => x.CardNumber.Equals(cardNumber)).Password == password;
         }
     }
 }
