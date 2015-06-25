bb462b8dcfa2bf987168008e8a9bd386b9e3787b
LeroyDjenkins <another0ne@mail.ru>
2015-06-25 19:02:46 +0300

    UserInterface(In process)

---
+++ ATM/ATM/Global.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+
+namespace ATM
+{
+    public static class Assets
+    {
+        public static string Cursor;
+        public static string Label;
+        public static string Background;
+    }
+
+    public static class Global
+    {
+        public static Session PlayerOne;
+    }
+}
---
+++ ATM/ATM/UserInterface/Cursor.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace ATM.UI
+{
+    class Cursor : Entity
+    {
+        private int _cooldown = 20;
+        public Cursor()
+        {
+            SetGraphic(new Image(Assets.Cursor));
+        }
+
+        public override void Update()
+        {
+            _cooldown--;
+            var Y = Game.Input.MouseY;
+            var X = Game.Input.MouseX;
+
+            Graphic.X = X;
+            Graphic.Y = Y;
+            if (_cooldown == 0)
+            {
+                RemoveSelf();
+                GameScene.Instance.Add(new Cursor());
+            }
+        }
+    }
+}
