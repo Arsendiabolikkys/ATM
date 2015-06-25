821bd8ab7eccd1540030e808444597a189fc2d6a
LeroyDjenkins <another0ne@mail.ru>
2015-06-25 18:08:31 +0300

    Task.Start()

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("Game", 1920, 1080); //creates a game with internal resolution 1920 x 1080
+            game.SetWindow(1600, 900); //outputs the game to a window scaled down to 1600 x 900
+
+            game.Start(); //starts the game loop
+        }
+    }
+}
